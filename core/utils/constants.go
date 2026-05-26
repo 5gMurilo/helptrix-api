b0fa7479999ef17bb014a3d922149cd92736b8a0
Murilo Cardoso <65872392+5gMurilo@users.noreply.github.com>
2026-05-26 20:50:27 -0300

    update regex validations

--- core/utils/constants.go
+++ core/utils/constants.go
@@ -27,8 +27,8 @@ const (
 )
 
 var (
-	CPFRegex      = regexp.MustCompile(`^\b{11|16)$`)
-	CNPJRegex     = regexp.MustCompile(`^\b(11|20)$`)
+	CPFRegex      = regexp.MustCompile(`^(\d{11}|\d{16})$`)
+	CNPJRegex     = regexp.MustCompile(`^(\d{11}|\d{20}$`)
 	TimeHHMMRegex = regexp.MustCompile(`^([01]\d|2[0-3]):[0-5]\d$`)
 
 	ErrUserAlreadyRegistered     = errors.New("user already registered with this email and role")
